3d22e7e805a5a84efdbc71a8563581480a1679e0
Chad Supers <Chad@Chads-MacBook-Air.local>
2016-08-08 18:46:21 -0700

    Final Push

--- js/main.js
+++ js/main.js
@@ -1,27 +1,10 @@
-// var stickyNavOffset = $('#logo').offset().top;
-
-
-// $(window).on('scroll', function() {
-// 	var currentScroll = $(window).scrollTop();
-	
-// 	if ( currentScroll >= stickyNavOffset) {
-// 		$('#logo').addClass('scrolledLogo');
-// 		// $('.main-nav').css('background-color','#F07057');
-
-// 	}
-
-// 	else {
-// 		$('#logo').removeClass('scrolledLogo');
-// 		// $('.main-nav').css('background-color','white');
-// 	}
-
-// });
+// STICKY NAV
 
 var stickyNavOffset2 = $('#formWrapper').offset().top;
 
 
 $(window).on('scroll', function() {
-	var currentScroll2 = $(window).scrollTop();
+	var currentScroll2 = $(window).scrollTop()+62;
 	
 	if ( currentScroll2 >= stickyNavOffset2) {
 		$('#formWrapper').addClass('scrolledForm');
@@ -37,3 +20,46 @@ $(window).on('scroll', function() {
 	}
 
 });
+
+
+
+
+// THIS IS THE JS FOR THE SCROLL TRANISITION
+
+$('header a').on('click', function(e) {
+  // prevent the standard link operation on click
+  e.preventDefault();
+  // use the href of the link to identify what
+  // section to scroll to
+  var thisTarget = $(this).attr('href');
+  // get that section's top offset
+  var targetOffset = $(thisTarget).offset().top;
+  // use jQuery.animate() to animate the body's
+  // scrollTop to the targetOffest
+  $('body').animate({
+    scrollTop: targetOffset
+  }, 600);
+});
+
+new WOW().init();
+
+// Regex for email validation
+
+function isValidEmailAddress(emailAddress) {
+    var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+    return pattern.test(emailAddress);
+}
+
+$("#mail").keyup(function() {
+	var emailaddress = $('#mail').val();
+	if( !isValidEmailAddress( emailaddress ) ) { 
+		$('#mail').addClass('badEmail')
+	}
+	else {
+		$('#mail').removeClass('badEmail')
+	}
+});
+
+$('#submit').on('click', function(e) {
+	  e.preventDefault();
+})
